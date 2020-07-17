ed302ec9d2215655e7dd69d696a57de4458819a2
Johnson4996 <Johnson.4996@gmail.com>
2020-07-17 10:15:35 -0500

    completed filtering for manufacturing businesses

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,8 +1,9 @@
-import { useBusiness, onlyNy } from "./BusinessProvider.js"
+import { useBusiness, onlyNy, onlyManuf } from "./BusinessProvider.js"
 import { businessHTML } from "./Business.js"
 
 const allBus = document.querySelector(".businessList")
 const nyBus = document.querySelector(".businessList--newYork")
+const manufBus = document.querySelector(".businessList--manufacturing")
 
 
 
@@ -24,4 +25,13 @@ export const businessNy =() =>{
     nyBusinesses.forEach(business => {
         nyBus.innerHTML += businessHTML(business)
     });
+}
+
+export const businnessManuf = () =>{
+    const busManuf = onlyManuf()
+    manufBus.innerHTML = "<h1> Businesses in Manufacturing </h1>"
+
+    busManuf.forEach(business =>{
+        manufBus.innerHTML += businessHTML(business)
+    })
 }
